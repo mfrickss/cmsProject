311e361e2b102e5d7f9c2869b556697645de6e5d
mfrickss <mfricks777@gmail.com>
2025-11-04 00:26:52 -0300

    add: schemas & crud

---
+++ backend/app/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+from typing import Optional
+
+class UserBase(BaseModel):
+    email: EmailStr
+    name: str
+    age: Optional[int] = None
+
+class UserCreate(UserBase):
+    password: str
+
+class UserOut(UserBase):
+    id: int
+    is_active: bool
+
+    model_config = ConfigDict(from_attributes=True)
+
+class Token(BaseModel):
+    acess_token = str
+    token_type = str
+
+class TokenData(BaseModel):
+    username = str | None = None
