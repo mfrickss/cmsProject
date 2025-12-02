f9a108fd365e7900bb8203f09ebbcba88bcfbbbb
Ricardo Augusto de Camargo <est.ricardoac@EPGE056696.PGESL>
2025-12-02 14:21:38 -0300

    feat: add tokens jwt

---
+++ backend/app/auth.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from pydantic import BaseModel
+
+SECRET_KEY = "a4cb36a1a6036bdb924137c915096170d9690d1d95e77c4f7c9c4fa7994e384f"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username = str | None = None
+
+def create_acess_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
--- backend/app/main.py
+++ backend/app/main.py
@@ -4,3 +4,11 @@ app = FastAPI(title="Meu CMS API")
 @app.get("/")
 def read_root():
     return {"Hello": "World", "message": "API está rodando!"}
+
+app.post("/register")
+def user_register():
+    return {"sexo"}
+
+app.get("/login")
+def user_register():
+    return {"sexo"}
